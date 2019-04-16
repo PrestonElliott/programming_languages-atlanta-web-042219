6a1be24b4a231a713056713822bd849de0267b7e
Preston Elliott <preston.elliott24@gmail.com>
2019-04-16 11:38:49 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,45 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |oo_functional, language|
+    language.each do |language, type|
+      type.each do |type, string|
+        if new_hash[language].nil?
+          new_hash[language] = {}
+        end
+        new_hash[language][:style] ||= []
+        new_hash[language][:style] << oo_functional
+        if new_hash[language][type].nil?
+          new_hash[language][type] = string
+        end
+      end
+    end
+  end
+  new_hash
 end
+
+# target format
+# {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#     :style => [:oo, :functional]
+#   },
+#   :python => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+
+# example - adding nested hash values
+# hash = {}
+# hash[:my_key] = {second_level_key: "second level value"}
+#
+# puts hash
+# #  =>
+# {
+#   my_key: {
+#      second_level_key: "second level value"
+#   }
+# }
